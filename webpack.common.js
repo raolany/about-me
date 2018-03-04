4031fe614bd7ab6a246e8d6a1e32d5a590a0ddbf
Andrii Rak <andrii.rack@gmail.com>
2018-03-04 21:26:54 +0200

    favicon

--- webpack.common.js
+++ webpack.common.js
@@ -54,7 +54,8 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: 'src/index.html',
             inject: 'body',
-            filename: 'index.html'
+            filename: 'index.html',
+            favicon: 'src/images/favicon.png'
         }),
     ]
 }
